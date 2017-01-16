243e5009747b520f2597dab5aee5ba0fb63ff0ee
redacid <redacid@redacid.org.ua>
2017-01-16 14:06:21 +0200

    adapted for nagios

--- check-certs.go
+++ check-certs.go
@@ -19,8 +19,8 @@ import (
 //States for exit codes, need by nagios
 const STATE_OK = 0
 const STATE_WARNING = 1
-const STATE_CRITICAL = 2
-const STATE_UNKNOWN = 3
+const STATE_CRITICAL = 2 //not used
+const STATE_UNKNOWN = 3 //not used
 
 const defaultConcurrency = 8
 
