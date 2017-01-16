21a1d31ffdc6eedc02a6e04daadc535a8f13594a
redacid <redacid@redacid.org.ua>
2017-01-16 12:06:27 +0200

    test

--- check-certs.go
+++ check-certs.go
@@ -140,17 +140,17 @@ func processHosts() {
 			fmt.Printf("%s: %v\n", r.host, r.err)
 			continue
 			state = STATE_CRITICAL;
-			fmt.Printf("%d\n",state);
+			//fmt.Printf("%d\n",state);
 		}
 		for _, cert := range r.certs {
 			for _, err := range cert.errs {
 				fmt.Println(err)
 				state = STATE_CRITICAL;
-				fmt.Printf("%d\n",state);
+				//fmt.Printf("%d\n",state);
 			}
 		}
 	}
-	fmt.Printf("%d\n",state);
+	//fmt.Printf("%d\n",state);
 	os.Exit(state);
 }
 
