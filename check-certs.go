8d3f783dba346eced8f2a1766d9f9b4509c62c6f
Serhii Rudenko <serhii.rudenko@prozorro.sale>
2025-02-18 11:02:15 +0200

    edt output

--- check-certs.go
+++ check-certs.go
@@ -25,11 +25,11 @@ const STATE_UNKNOWN = 3 //not used
 const defaultConcurrency = 8
 
 const (
-	errExpiringShortly = "%s: ** '%s' expires in %d hours! **"
+	errExpiringShortly = "%s: ** '%s' %s expires in %d hours! **"
 	//errExpiringShortly = "%s: ** '%s' (S/N %X) expires in %d hours! **"
-	errExpiringSoon    = "%s: '%s' expires in roughly %d days."
+	errExpiringSoon    = "%s: '%s' %s expires in roughly %d days."
 	//errExpiringSoon    = "%s: '%s' (S/N %X) expires in roughly %d days."
-	errSunsetAlg       = "%s: '%s' (S/N %X) expires after the sunset date for its signature algorithm '%s'."
+	errSunsetAlg       = "%s: '%s' %s (S/N %X) expires after the sunset date for its signature algorithm '%s'."
 )
 
 var state int=STATE_OK;
